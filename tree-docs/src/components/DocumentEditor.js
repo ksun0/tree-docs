1c5f3aa1abff3eb646498c8f702c423ff1db96c0
Brian Bae <baebrian07@gmail.com>
2022-03-24 01:58:46 -0400

    asdf

--- tree-docs/src/components/DocumentEditor.js
+++ tree-docs/src/components/DocumentEditor.js
@@ -18,12 +18,18 @@ function DocumentEditor() {
 
     return (
         <>
-        <NavBar/>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <h2 align="center">Edit</h2>
+        <NavBar light={true}/>
+        <div id="hero" class="hero overlay subpage-hero blog-hero">
+            <div class="hero-content">
+                <div class="hero-text">
+                    <h1>Edit Document</h1>
+                    <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="/">Home</a></li>
+                    <li class="breadcrumb-item">Documents</li>
+                    </ol>
+                    </div>
+                </div>
+            </div>
         <main id="main" class="site-main">
 
         <section class="site-section subpage-site-section section-blog">
@@ -32,12 +38,12 @@ function DocumentEditor() {
                     <div class="col-md-8">
                         <form>
                             <div class="form-group">
-                            <label for="title">Title:</label>
+                            <label style={{fontSize: "20px", paddingBottom: "5px"}}for="title">Title:</label>
                               <textarea class="form-control form-control-title" id="title"></textarea>
                             </div>
                             <br></br>
                             <div class="form-group">
-                              <label for="body">Body:</label>
+                              <label style={{fontSize: "20px", paddingBottom: "5px"}} for="body">Body:</label>
                               <textarea class="form-control form-control-body" id="body"></textarea>
                             </div>
                         </form>
